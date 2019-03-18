54270336f108f905e6bcc581525f08a6e58cd0ee
Henrik Loeser <hloeser@de.ibm.com>
2019-03-18 14:56:48 +0100

    switch maintenance on/off

---
+++ scripts/vpc-maintenance.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Script related to an IBM Cloud solution tutorial
+# Enable / disable the maintenance security group
+#
+# (C) 2019 IBM
+#
+# Written by Henrik Loeser, hloeser@de.ibm.com
+
+if [ -z "$3" ]; then 
+              echo "usage: $0 vpc-name instance-name (on | off)"
+              exit
+fi
+
+ACTION=$3
+INSTANCE=$2
+VPCNAME=$1
+
+
+
+# Obtain NIC ID for instance
+export NICID=$(ibmcloud is instances --json |\
+       jq -c -r '.[] | select (.vpc.name=="'${VPCNAME}'" and .name=="'${INSTANCE}'") | .primary_network_interface.id')
+
+export SGMAINT=$(ibmcloud is security-groups --json |\
+       jq -c -r '.[] | select (.vpc.name=="'${VPCNAME}'" and .name=="'${VPCNAME}'-maintenance-sg") | .id')
+
+if [ $ACTION = "on" ]; then
+    ibmcloud is security-group-network-interface-add $SGMAINT $NICID
+fi
+if [ $ACTION = "off" ]; then
+    ibmcloud is security-group-network-interface-remove $SGMAINT $NICID
+fi
