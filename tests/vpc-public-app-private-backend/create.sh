7ad214e668909fc293ccbacb3f2762d62e28cd4c
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2019-03-29 11:27:48 +0100

    new tests

---
+++ tests/vpc-public-app-private-backend/create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# deploy to first zone in the selected region
+ZONE=$(ibmcloud is zones $REGION --json | jq -r .[].name | sort | head -1)
+echo "Region is $REGION, zone is $ZONE"
+
+#./vpc-public-app-private-backend/vpc-pubpriv-create-with-bastion.sh $ZONE $KEYS automated-tests $RESOURCE_GROUP
