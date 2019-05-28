a4e0f76b3052fb49f88db4e5672c872cf9e93bc5
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2019-05-28 19:35:20 -0400

    Update post-build.sh
    - cleaned some informational messages

--- vpc-cockroachdb-mzr/post-build.sh
+++ vpc-cockroachdb-mzr/post-build.sh
@@ -6,9 +6,9 @@ floating_ip=$(jq -r '.vpc[].virtual_server_instances[]? | select(.type == "cockr
 
 log_info ""
 
-log_success "Access the admin instance using the following SSH command:"
+log_success "You can access the admin instance using the following SSH command:"
 log_warning "    ssh -F vpc-cockroachdb-mzr/ssh-init/ssh.config root@${floating_ip}"
-log_info ""
+log_success ""
 
 for vsi_list in $(jq -c '.vpc[]?.virtual_server_instances[]? | select(.type == "cockroachdb")' ${configFile}); do
     vsi_name_temp=$(echo ${vsi_list} | jq -r '.name | select (.!=null)')
@@ -21,16 +21,16 @@ for vsi_list in $(jq -c '.vpc[]?.virtual_server_instances[]? | select(.type == "
         fi
     done    
 
-    log_success "Access the cockroachdb node ${vsi_name} using the following SSH command:"
+    log_success "You can access the cockroachdb node ${vsi_name} using the following SSH command:"
     log_warning "    ssh -F vpc-cockroachdb-mzr/ssh-init/ssh.config -J root@${floating_ip} root@${primary_ipv4_address}"
-    log_info ""
+    log_success ""
 
-    log_success "Access the CockroachDB Web Admin UI on node ${vsi_name}, using the following SSH command:"
+    log_success "You can access the CockroachDB Web Admin UI on node ${vsi_name}, using the following SSH command:"
     log_warning "    ssh -F vpc-cockroachdb-mzr/ssh-init/ssh.config -L 8080:${primary_ipv4_address}:8080 root@${floating_ip}"
-    log_info ""
+    log_success ""
     log_success " and by pointing your browser to:"
     log_warning "    http://localhost" 
-    log_info ""    
+    log_success ""    
 done
 
 for vsi_list in $(jq -c '.vpc[]?.virtual_server_instances[]? | select(.type == "app")' ${configFile}); do
@@ -44,24 +44,9 @@ for vsi_list in $(jq -c '.vpc[]?.virtual_server_instances[]? | select(.type == "
         fi
     done 
 
-    log_success "Access the app node ${vsi_name} using the following SSH command:"
+    log_success "You can access the app node ${vsi_name} using the following SSH command:"
     log_warning "    ssh -F vpc-cockroachdb-mzr/ssh-init/ssh.config -J root@${floating_ip} root@${primary_ipv4_address}"
-    log_info ""   
-
-#     lb_hostname=$(jq -c '.vpc[]?.load_balancers[]? | select(.type == "private") | .hostname | select(.!=null)' ${configFile})
-
-# cat > "${config_template_file_dir}/local/${primary_ipv4_address}.cockroachdb.json" <<- EOF
-# {
-#   "address": $lb_hostname
-# }
-# EOF
-
-#     log_info "${BASH_SOURCE[0]}: Copying app configuration to node ${primary_ipv4_address} using ${floating_ip} as jump host."
-#     scp -F "${config_template_file_dir}/ssh-init/ssh.config" -r -o "ProxyJump root@${floating_ip}" "${config_template_file_dir}/local/${primary_ipv4_address}.cockroachdb.json" root@${primary_ipv4_address}:/vpc-tutorials/apps/nodejs-graphql-cockroachdb/config/cockroachdb.json
-#     [ $? -ne 0 ] && log_error "${BASH_SOURCE[0]}: Error copying app configuration to node ${primary_ipv4_address}." && return 1
-
-#     log_success "Starting the NodeJS sample app on node ${vsi_name} using ${floating_ip} as jump host."
-#     ssh -F "${config_template_file_dir}/ssh-init/ssh.config" -J root@${floating_ip} root@${primary_ipv4_address} -t 'cd /vpc-tutorials/apps/nodejs-graphql-cockroachdb/ && npm start'
+    log_success ""   
 
 done
 
@@ -79,7 +64,7 @@ for load_balancer in $(jq -c '.vpc[]?.load_balancers[]?' ${configFile} | tr -d '
             fi
         done
 
-        log_success "Access the ${load_balancer_type} load balancer ${load_balancer_name} using the following address:"
+        log_success "The ${load_balancer_type} load balancer ${load_balancer_name} is available using the following address:"
         log_warning "    ${load_balancer_hostname}"
         
         for load_balancer_listener in ${load_balancer_listeners}; do
@@ -88,16 +73,16 @@ for load_balancer in $(jq -c '.vpc[]?.load_balancers[]?' ${configFile} | tr -d '
 
             log_success " on ${listener_protocol} port:"
             log_warning "    ${listener_port}"
-            log_info "" 
+            log_success "" 
         done
 
     fi
 done
 
 log_success "After having created the database and table as per the instructions found here:"
-log_warning "    https://github.ibm.com/dimitri-prosper/snowball/tree/master/vpc-cockroachdb-mzr#test-the-cluster-taken-from-the-cockroachdb-documentation"
+log_warning "    https://github.com/IBM-Cloud/vpc-tutorials/tree/experimental/vpc-cockroachdb-mzr#test-the-cluster-taken-from-the-cockroachdb-documentation"
 
-log_success " access a sample NodeJS app that interacts with the database:"
+log_success "you may access a sample NodeJS app that interacts with the database:"
 log_warning "    http://${load_balancer_hostname}/api/bank"
 
 log_info "${BASH_SOURCE[0]}: Running post-build script. done."
