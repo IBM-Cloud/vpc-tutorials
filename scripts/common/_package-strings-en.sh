aaa2cc10eb9dd65845b958b9540f06018b514ae1
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2019-06-17 15:33:39 -0400

    Update _package-strings-en.sh
    - added additional help description.

--- scripts/common/_package-strings-en.sh
+++ scripts/common/_package-strings-en.sh
@@ -6,6 +6,6 @@ jq_missing_msg2="Download the latest version for your operating system here: htt
 usage_msg1="Please supply all required parameters --config=<filename>.json --template=<filename>.json."
 usage_msg1_delete="Please supply all required parameters --config=<filename>.json --template=<filename>.json.."
 
-package_info_missing_msg1="This script cannot run without a valid package-info.json."
+package_info_missing_msg1="This script cannot run without a valid package-info.json. Make sure you are running the script from the same directory where the package-info.json and other supporting files are located."
 package_info_missing_scriptname_msg1="The package-info.json is missing a script name."
 package_info_missing_version_msg1="The package-info.json is missing a script version."
