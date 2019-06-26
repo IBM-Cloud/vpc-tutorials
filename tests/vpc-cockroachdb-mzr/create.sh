2824af8967daec3c75d19cfe3db601a7730a3470
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2019-06-26 14:51:41 +0200

    run cockroachdb script

---
+++ tests/vpc-cockroachdb-mzr/create.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+# generate the config file
+echo '{
+  "resources_prefix": "'$JOB_ID'",
+  "region": "'$REGION'",
+  "resource_group": "'$RESOURCE_GROUP'",
+  "x_use_resources_prefix": "vpc",
+  "ssh_keys": [' > ./vpc-cockroachdb-mzr/test.json
+
+keynames=$KEYS
+keys=()
+while [ "$keynames" ] ;do
+  iter=${keynames%%,*}
+  echo -n '    {
+      "name": "'$iter'",
+      "type": "vpc"
+    }' >> ./vpc-cockroachdb-mzr/test.json
+  [ "$keynames" = "$iter" ] && \
+  keynames='' || \
+  keynames="${keynames#*,}"
+  if [ "$keynames" ]; then
+    echo ',' >> ./vpc-cockroachdb-mzr/test.json
+  fi
+done
+
+echo '
+  ]
+}' >> ./vpc-cockroachdb-mzr/test.json
+
+cp ./vpc-cockroachdb-mzr/vpc-cockroachdb-mzr.template.json \
+  ./vpc-cockroachdb-mzr/vpc-cockroachdb-mzr.test.json
+sed -i 's/vpc-cockroachdb/'$TEST_VPC_NAME$'/g' ./vpc-cockroachdb-mzr/vpc-cockroachdb-mzr.test.json
+
+# deploy the config
+./build.sh --template=./vpc-cockroachdb-mzr/vpc-cockroachdb-mzr.test.json --config=./vpc-cockroachdb-mzr/test.json
---
+++ tests/vpc-cockroachdb-mzr/teardown.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./delete.sh --template=./vpc-cockroachdb-mzr/vpc-cockroachdb-mzr.test.json --config=./vpc-cockroachdb-mzr/test.json
