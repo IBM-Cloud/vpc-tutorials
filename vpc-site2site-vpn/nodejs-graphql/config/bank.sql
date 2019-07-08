abd8053490edf2edafd98f14569596336e4d3966
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2019-07-08 10:50:03 -0400

    app moved to sampleapps directory as it is shared with other tutorials.

--- vpc-site2site-vpn/nodejs-graphql/config/bank.sql
+++
@@ -1,2 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-CREATE TABLE IF NOT EXISTS accounts (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), transactiontime TIMESTAMPTZ DEFAULT current_timestamp,  balance DECIMAL); 
