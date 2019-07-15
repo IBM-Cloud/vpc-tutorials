4d510211f29ac767b36e2971b76a09290c60117c
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2019-07-15 11:40:08 -0400

    Update vpc_subnets.tf
    - corrected the name of the cloud subnet

--- vpc-site2site-vpn/tf/vpc_subnets.tf
+++ vpc-site2site-vpn/tf/vpc_subnets.tf
@@ -1,6 +1,6 @@
 resource ibm_is_subnet "sub_cloud" {
   count                    = 1
-  name                     = "${var.resources_prefix}-cloud-subnet-${count.index + 1}"
+  name                     = "${var.resources_prefix}-cloud-subnet"
   vpc                      = "${ibm_is_vpc.vpc.id}"
   zone                     = "${lookup(var.vpc_zones, "${var.vpc_region}-availability-zone-2")}"
   total_ipv4_address_count = 16
