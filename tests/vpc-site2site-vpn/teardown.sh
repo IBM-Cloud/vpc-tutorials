86ab8e3a79c4613f51bb626a27d60d94dfc87db7
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2019-07-17 17:11:27 +0200

    cleanup classic key only

--- tests/vpc-site2site-vpn/teardown.sh
+++ tests/vpc-site2site-vpn/teardown.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # include common functions
-. $(dirname "$0")/../../scripts/common.sh
+this_dir=$(dirname "$0")
+source $this_dir/../tests_common.sh
 
-# remove the key created for the test
-export TEST_KEY_NAME="automated-tests-${JOB_ID}"
-export TEST_KEY_ID=$(SSHKeynames2UUIDs $TEST_KEY_NAME)
-ibmcloud is key-delete $TEST_KEY_ID --force
-
+# remove the classic infrastructure key created for the test
+export TEST_KEY_NAME=$(ssh_key_name_for_job)
 CLASSIC_SSH_KEYS=$(ibmcloud sl call-api SoftLayer_Account getSshKeys)
 CLASSIC_SSH_KEY_TO_DELETE=$(echo $CLASSIC_SSH_KEYS | jq -r '.[] | select(.label=="'$TEST_KEY_NAME'") | .id')
 ibmcloud sl security sshkey-remove $CLASSIC_SSH_KEY_TO_DELETE -f
