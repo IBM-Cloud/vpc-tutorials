e5930dab9adf41f8cfe88af7da51b77e876a7f74
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2019-07-17 14:44:42 -0400

    cleanup for redo

--- vpc-site2site-vpn/tf/modules/vpc_bastion_tf/output.tf
+++
@@ -1,7 +0,0 @@
-output "vpc_vsi_bastion_fip" {
-  value = "${ibm_is_floating_ip.vpc_vsi_bastion_fip.0.address}"
-}
-
-output sg_maintenance_id {
-    value = "${ibm_is_security_group.sg_maintenance.id}"
-}
