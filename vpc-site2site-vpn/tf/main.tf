cd57ab688f350e6fdb87258903ff29b1f7b029b6
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2019-07-18 15:44:00 -0400

    redo to fit guidance from vpc-public-app-private-backend terraform example

--- vpc-site2site-vpn/tf/main.tf
+++ vpc-site2site-vpn/tf/main.tf
@@ -1,41 +1,210 @@
 provider "ibm" {
-  region          = "us-south"
+  # region          = "us-south"
+  region          = "eu-gb"
   ibmcloud_api_key = "${var.ibmcloud_api_key}"
   generation = 1 
+  softlayer_username = "${var.softlayer_username}"
+  softlayer_api_key  = "${var.softlayer_api_key}"
 }
 locals {
-  BASENAME = "${var.prefix}vpc-pubpriv"
-  user_data_frontend = <<EOF
+  BASENAME = "${var.prefix}-vpc"
+
+  user_data_cloud = <<EOF
 #!/bin/bash
 apt-get update
-apt-get install -y nginx
-echo "I am the frontend server" > /var/www/html/index.html
-service nginx start
-EOF
-  user_data_backend = <<EOF
-#!/bin/bash
-apt-get update
-apt-get install -y nginx
-echo "I am the backend server" > /var/www/html/index.html
-service nginx start
+apt-get install -y nodejs npm
 EOF
 }
-module vpc_pub_priv {
-  source = "../tfmodule"
+
+data "ibm_resource_group" "all_rg" {
+  name = "${var.resource_group_name}"
+}
+resource "ibm_is_vpc" "vpc" {
+  name = "${var.vpc_name == "" ? local.BASENAME : var.vpc_name}"
+  resource_group = "${data.ibm_resource_group.all_rg.id}"
+}
+resource "ibm_is_public_gateway" "cloud" {
+    count = "${var.cloud_pgw?1:0}"
+    vpc             = "${ibm_is_vpc.vpc.id}"
+    name = "${local.BASENAME}-${var.zone_vsi}-pubgw"
+    zone            = "${var.zone_vsi}"
+}
+
+resource "ibm_is_public_gateway" "bastion" {
+    count = "${var.bastion_pgw?1:0}"
+    vpc             = "${ibm_is_vpc.vpc.id}"
+    name = "${local.BASENAME}-${var.zone_bastion}-pubgw"
+    zone            = "${var.zone_bastion}"
+}
+
+resource "ibm_is_subnet" "cloud" {
+  name            = "${local.BASENAME}-cloud-subnet"
+  vpc             = "${ibm_is_vpc.vpc.id}"
+  zone            = "${var.zone_vsi}"
+  public_gateway = "${join("", ibm_is_public_gateway.cloud.*.id)}"
+  total_ipv4_address_count = 256
+}
+
+# bastion subnet and instance values needed by the bastion module
+resource "ibm_is_subnet" "bastion" {
+  name            = "${local.BASENAME}-bastion-subnet"
+  vpc             = "${ibm_is_vpc.vpc.id}"
+  zone            = "${var.zone_bastion}"
+  total_ipv4_address_count = 256
+}
+data "ibm_is_image" "os" {
+  name = "${var.image_name}"
+}
+data "ibm_is_ssh_key" "sshkey" {
+  name = "${var.ssh_key_name}"
+}
+
+locals {
+  bastion_ingress_cidr = "0.0.0.0/0" # DANGER: cidr range that can ssh to the bastion when maintenance is enabled
+  maintenance_egress_cidr = "0.0.0.0/0" # cidr range required to contact software repositories when maintenance is enabled
+}
+module bastion {
+  source = "../../vpc-secure-management-bastion-server/tfmodule"
   basename = "${local.BASENAME}"
-  vpc_name = "${var.vpc_name}"
-  resource_group_name = "${var.resource_group_name}"
-  ssh_key_name = "${var.ssh_key_name}"
-  zone = "${var.zone}"
-  backend_pgw = "${var.backend_pgw}"
+  ibm_is_vpc_id = "${ibm_is_vpc.vpc.id}"
+  zone            = "${var.zone_bastion}"
+  remote = "${local.bastion_ingress_cidr}"
   profile = "${var.profile}"
-  image_name = "${var.image_name}"
-  maintenance = "${var.maintenance}"
-  frontend_user_data = "${local.user_data_frontend}"
-  backend_user_data = "${local.user_data_backend}"
+  ibm_is_image_id = "${data.ibm_is_image.os.id}"
+  ibm_is_ssh_key_id = "${data.ibm_is_ssh_key.sshkey.id}"
+  ibm_is_subnet_id = "${ibm_is_subnet.bastion.id}"
 }
+# maintenance will require ingress from the bastion, so the bastion has output a maintenance SG
+# maintenance may also include installing new versions of open source software that are not in the IBM mirrors
+# add the additional egress required to the maintenance security group exported by the bastion
+# for example at 53 DNS, 80 http, and 443 https probably make sense
+resource "ibm_is_security_group_rule" "maintenance_egress_443" {
+  group     = "${module.bastion.security_group_id}"
+  direction = "egress"
+  remote    = "${local.maintenance_egress_cidr}"
+  tcp = {
+    port_min = "443"
+    port_max = "443"
+  }
+}
+resource "ibm_is_security_group_rule" "maintenance_egress_80" {
+  group     = "${module.bastion.security_group_id}"
+  direction = "egress"
+  remote    = "0.0.0.0/0"
+  tcp = {
+    port_min = 80
+    port_max = 80
+  }
+}
+resource "ibm_is_security_group_rule" "maintenance_egress_53" {
+  group     = "${module.bastion.security_group_id}"
+  direction = "egress"
+  remote    = "0.0.0.0/0"
+  tcp = {
+    port_min = 53
+    port_max = 53
+  }
+}
+resource "ibm_is_security_group_rule" "maintenance_egress_udp_53" {
+  group     = "${module.bastion.security_group_id}"
+  direction = "egress"
+  remote    = "0.0.0.0/0"
+  udp = {
+    port_min = 53
+    port_max = 53
+  }
+}
+
+resource "ibm_is_security_group" "cloud" {
+  name = "${local.BASENAME}-sg"
+  vpc  = "${ibm_is_vpc.vpc.id}"
+}
+
+resource "ibm_is_security_group_rule" "cloud_ingress_tcp_80" {
+  group     = "${ibm_is_security_group.cloud.id}"
+  direction = "ingress"
+  remote    = "0.0.0.0/0"
+  tcp = {
+    port_min = 80
+    port_max = 80
+  }
+}
+
+resource "ibm_is_security_group_rule" "cloud_ingress_tcp_443" {
+  group     = "${ibm_is_security_group.cloud.id}"
+  direction = "ingress"
+  remote    = "0.0.0.0/0"
+  tcp = {
+    port_min = 443
+    port_max = 443
+  }
+}
+
+resource "ibm_is_security_group_rule" "cloud_ingress_tcp_22" {
+  group     = "${ibm_is_security_group.cloud.id}"
+  direction = "ingress"
+  remote    = "0.0.0.0/0"
+  tcp = {
+    port_min = 22
+    port_max = 22
+  }
+}
+
+resource "ibm_is_security_group_rule" "cloud_ingress_icmp_8" {
+  group     = "${ibm_is_security_group.cloud.id}"
+  direction = "ingress"
+  remote    = "0.0.0.0/0"
+
+  icmp = {
+    type = 8
+  }
+}
+
+resource "ibm_is_security_group_rule" "cloud_egress_tcp_all" {
+  group     = "${ibm_is_security_group.cloud.id}"
+  direction = "egress"
+  remote    = "0.0.0.0/0"
+}
+
+#Cloud
 locals {
-  bastion_ip = "${module.vpc_pub_priv.bastion_floating_ip_address}"
+  # create either [cloud] or [cloud, maintenance] depending on the var.maintenance boolean
+  cloud_security_groups = "${split(",", var.maintenance ? format("%s,%s", ibm_is_security_group.cloud.id, module.bastion.security_group_id) : ibm_is_security_group.cloud.id)}"
+}
+resource "ibm_is_instance" "cloud" {
+  name    = "${local.BASENAME}-cloud-vsi"
+  image   = "${data.ibm_is_image.os.id}"
+  profile = "${var.profile}"
+  vpc       = "${ibm_is_vpc.vpc.id}"
+  zone      = "${var.zone_vsi}"
+  keys      = ["${data.ibm_is_ssh_key.sshkey.id}"]
+  user_data = "${local.user_data_cloud}"
+
+  primary_network_interface = {
+    subnet          = "${ibm_is_subnet.cloud.id}"
+    security_groups = ["${local.cloud_security_groups}"]
+  }
+}
+
+
+data "ibm_compute_ssh_key" "sshkey" {
+    label =  "${var.softlayer_ssh_key_name}"
+}
+
+# Create a virtual server with the SSH key
+resource "ibm_compute_vm_instance" "onprem" {
+  hostname          = "${local.BASENAME}-onprem-vsi"
+  domain            = "solution-tutorial.cloud.ibm"
+  ssh_key_ids       = ["${data.ibm_compute_ssh_key.sshkey.id}"]
+  os_reference_code = "${var.softlayer_image_name}"
+  datacenter        = "${var.softlayer_datacenter}"
+  network_speed     = 100
+  cores             = 1
+  memory            = 1024
+}
+
+locals {
+  bastion_ip = "${module.bastion.floating_ip_address}"
 }
 output "BASTION_IP_ADDRESS" {
   value = "${local.bastion_ip}"
@@ -43,21 +212,18 @@ output "BASTION_IP_ADDRESS" {
 output "sshbastion" {
   value = "ssh root@${local.bastion_ip}"
 }
-output "sshfrontend" {
-  value = "ssh -o ProxyJump=root@${local.bastion_ip} root@${module.vpc_pub_priv.frontend_network_interface_address}"
+output "sshcloud" {
+  value = "ssh -o ProxyJump=root@${local.bastion_ip} root@${ibm_is_instance.cloud.primary_network_interface.0.primary_ipv4_address}"
 }
-output "FRONT_IP_ADDRESS" {
-  value = "${module.vpc_pub_priv.frontend_floating_ip_address}"
+output "CLOUD_CIDR" {
+  value = "${ibm_is_subnet.cloud.ipv4_cidr_block}"
 }
-output "FRONT_NIC_IP" {
-  value = "${module.vpc_pub_priv.frontend_network_interface_address}"
+output "VSI_CLOUD_IP" {
+  value = "${ibm_is_instance.cloud.primary_network_interface.0.primary_ipv4_address}"
 }
-output "sshbackend" {
-  value = "ssh -o ProxyJump=root@${local.bastion_ip} root@${module.vpc_pub_priv.backend_network_interface_address}"
+output "ONPREM_CIDR" {
+  value = "${ibm_compute_vm_instance.onprem.private_subnet}"
 }
-output "BACK_IP_ADDRESS" {
-  value = "${module.vpc_pub_priv.backend_floating_ip_address}"
-}
-output "BACK_NIC_IP" {
-  value = "${module.vpc_pub_priv.backend_network_interface_address}"
+output "VSI_ONPREM_IP" {
+  value = "${ibm_compute_vm_instance.onprem.ipv4_address}"
 }
--- vpc-site2site-vpn/tf/variables.tf
+++ vpc-site2site-vpn/tf/variables.tf
@@ -1,32 +1,104 @@
 # see export.template for these:
 variable ibmcloud_api_key { }
+
+# ssh key name the string 'pfq' in the example below:
+# $ ibmcloud is keys
+# Listing keys under account Powell Quiring's Account as user pquiring@us.ibm.com...
+# ID                                     Name   Type   Length   FingerPrint          Created
+# 636f6d70-0000-0001-0000-00000014f113   pfq    rsa    4096     vaziuuZ4/BVQrgFO..   2 months ago
 variable "ssh_key_name" { }
 
 # resources created will be named: ${prefix}vpc-pubpriv, vpc name will be ${prefix} or will be defined by vpc_name
 variable "prefix" {
-  default = "tf00"
+  default = "vpns2s"
 }
+
 # if this is empty use the basename for the vpc name.  If not empty then use this for the vpc_name
 variable "vpc_name" {
   default = ""
 }
-# These variables are well documented in the ../tfmodule/variable.tf file.
+
+# zone string, us-south-1, in the example below
+# $ ibmcloud is zones
+# Listing zones in target region us-south under account Powell Quiring's Account as user pquiring@us.ibm.com...
+# Name         Region     Status   
+# us-south-3   us-south   available   
+# us-south-1   us-south   available   
+# us-south-2   us-south   available   
+
 variable "zone" {
   default = "us-south-1"
 }
-variable "backend_pgw" {
+variable "zone_bastion" {
+  # default = "us-south-1"
+  default = "eu-gb-1"
+}
+
+variable "zone_vsi" {
+  # default = "us-south-2"
+  default = "eu-gb-2"
+}
+
+# set to true if the cloud should have a public gateway.  This is used to provision software.
+variable "cloud_pgw" {
+  default = true
+}
+
+# set to true if the bastion should have a public gateway.  This is used to provision software.
+variable "bastion_pgw" {
   default = false
 }
+# instance profile string, cc1-2x4, in the example below
+# $ ibmcloud is instance-profiles
+# Listing server profiles under account Powell Quiring's Account as user pquiring@us.ibm.com...
+# Name         Family
+# ...
+# cc1-2x4      cpu
 variable "profile" {
   default = "cc1-2x4"
 }
+
+# image name, centos-7.x-amd64, in the example below
+# $ ibmcloud is images
+# Listing images under account Powell Quiring's Account as user pquiring@us.ibm.com...
+# ID                                     Name                    OS                                                        Created        Status   Visibility
+# cc8debe0-1b30-6e37-2e13-744bfb2a0c11   centos-7.x-amd64        CentOS (7.x - Minimal Install)                            6 months ago   READY    public
+# cfdaf1a0-5350-4350-fcbc-97173b510843   ubuntu-18.04-amd64      Ubuntu Linux (18.04 LTS Bionic Beaver Minimal Install)    6 months ago   READY    public
+# ...
 variable "image_name" {
   default = "ubuntu-18.04-amd64"
 }
+# when true, both the frontend and backend instances will add the bastion maintenance security group
+# to their security group list, allowing ssh access from the bastion
 variable "maintenance" {
   default = true
 }
 
+# provide the cloud-init script, empty means none
+# variable frontend_user_data { }
+# variable backend_user_data { }
+
+# the backend security group allows ingress from the frontend for this port
+# The frontend security group allows egress to the backend for this port
+variable cloud_tcp_port {
+  default = 80
+}
+
+
 ##
 variable "resource_group_name" {
 }
+
+variable "softlayer_image_name" {
+  default = "Ubuntu_latest"
+}
+
+variable "softlayer_datacenter" {
+  default = "dal10"
+}
+
+variable "softlayer_ssh_key_name" { }
+
+variable "softlayer_username" { }
+
+variable "softlayer_api_key" { }
