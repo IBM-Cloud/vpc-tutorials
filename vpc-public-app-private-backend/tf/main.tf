780e74fd37cac3e408e9916c4870569846b861d2
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2019-07-23 10:12:36 +0200

    increase API timeout + fmt

--- vpc-public-app-private-backend/tf/main.tf
+++ vpc-public-app-private-backend/tf/main.tf
@@ -1,10 +1,13 @@
 provider "ibm" {
-  region          = "us-south"
+  region           = "us-south"
   ibmcloud_api_key = "${var.ibmcloud_api_key}"
-  generation = 1 
+  generation       = 1
+  ibmcloud_timeout = "${var.ibmcloud_timeout}"
 }
+
 locals {
   BASENAME = "${var.prefix}vpc-pubpriv"
+
   user_data_frontend = <<EOF
 #!/bin/bash
 apt-get update
@@ -12,6 +15,7 @@ apt-get install -y nginx
 echo "I am the frontend server" > /var/www/html/index.html
 service nginx start
 EOF
+
   user_data_backend = <<EOF
 #!/bin/bash
 apt-get update
@@ -20,44 +24,54 @@ echo "I am the backend server" > /var/www/html/index.html
 service nginx start
 EOF
 }
+
 module vpc_pub_priv {
-  source = "../tfmodule"
-  basename = "${local.BASENAME}"
-  vpc_name = "${var.vpc_name}"
+  source              = "../tfmodule"
+  basename            = "${local.BASENAME}"
+  vpc_name            = "${var.vpc_name}"
   resource_group_name = "${var.resource_group_name}"
-  ssh_key_name = "${var.ssh_key_name}"
-  zone = "${var.zone}"
-  backend_pgw = "${var.backend_pgw}"
-  profile = "${var.profile}"
-  image_name = "${var.image_name}"
-  maintenance = "${var.maintenance}"
-  frontend_user_data = "${local.user_data_frontend}"
-  backend_user_data = "${local.user_data_backend}"
+  ssh_key_name        = "${var.ssh_key_name}"
+  zone                = "${var.zone}"
+  backend_pgw         = "${var.backend_pgw}"
+  profile             = "${var.profile}"
+  image_name          = "${var.image_name}"
+  maintenance         = "${var.maintenance}"
+  frontend_user_data  = "${local.user_data_frontend}"
+  backend_user_data   = "${local.user_data_backend}"
 }
+
 locals {
   bastion_ip = "${module.vpc_pub_priv.bastion_floating_ip_address}"
 }
+
 output "BASTION_IP_ADDRESS" {
   value = "${local.bastion_ip}"
 }
+
 output "sshbastion" {
   value = "ssh root@${local.bastion_ip}"
 }
+
 output "sshfrontend" {
   value = "ssh -o ProxyJump=root@${local.bastion_ip} root@${module.vpc_pub_priv.frontend_network_interface_address}"
 }
+
 output "FRONT_IP_ADDRESS" {
   value = "${module.vpc_pub_priv.frontend_floating_ip_address}"
 }
+
 output "FRONT_NIC_IP" {
   value = "${module.vpc_pub_priv.frontend_network_interface_address}"
 }
+
 output "sshbackend" {
   value = "ssh -o ProxyJump=root@${local.bastion_ip} root@${module.vpc_pub_priv.backend_network_interface_address}"
 }
+
 output "BACK_IP_ADDRESS" {
   value = "${module.vpc_pub_priv.backend_floating_ip_address}"
 }
+
 output "BACK_NIC_IP" {
   value = "${module.vpc_pub_priv.backend_network_interface_address}"
 }
--- vpc-public-app-private-backend/tf/variables.tf
+++ vpc-public-app-private-backend/tf/variables.tf
@@ -1,32 +1,43 @@
 # see export.template for these:
-variable ibmcloud_api_key { }
-variable "ssh_key_name" { }
+variable ibmcloud_api_key {}
+
+variable "ssh_key_name" {}
+
+variable "ibmcloud_timeout" {
+  description = "Timeout for API operations in seconds."
+  default     = 900
+}
 
 # resources created will be named: ${prefix}vpc-pubpriv, vpc name will be ${prefix} or will be defined by vpc_name
 variable "prefix" {
   default = "tf00"
 }
+
 # if this is empty use the basename for the vpc name.  If not empty then use this for the vpc_name
 variable "vpc_name" {
   default = ""
 }
+
 # These variables are well documented in the ../tfmodule/variable.tf file.
 variable "zone" {
   default = "us-south-1"
 }
+
 variable "backend_pgw" {
   default = false
 }
+
 variable "profile" {
   default = "cc1-2x4"
 }
+
 variable "image_name" {
   default = "ubuntu-18.04-amd64"
 }
+
 variable "maintenance" {
   default = true
 }
 
 ##
-variable "resource_group_name" {
-}
+variable "resource_group_name" {}
