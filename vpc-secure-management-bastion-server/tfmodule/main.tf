342e88adbc28f9ec2e27f2a427dab8df52bebb09
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2019-07-23 10:10:37 +0200

    terraform fmt

--- vpc-secure-management-bastion-server/tfmodule/main.tf
+++ vpc-secure-management-bastion-server/tfmodule/main.tf
@@ -3,10 +3,12 @@ resource "ibm_is_security_group" "maintenance" {
   name = "${var.basename}-maintenance-sg"
   vpc  = "${var.ibm_is_vpc_id}"
 }
+
 resource "ibm_is_security_group_rule" "maintenance_ingress_ssh_bastion" {
   group     = "${ibm_is_security_group.maintenance.id}"
   direction = "ingress"
   remote    = "${ibm_is_security_group.bastion.id}"
+
   tcp = {
     port_min = 22
     port_max = 22
@@ -18,25 +20,30 @@ resource "ibm_is_security_group" "bastion" {
   name = "${var.basename}-bastion-sg"
   vpc  = "${var.ibm_is_vpc_id}"
 }
+
 # users of the bastian. for example from on premises
 resource "ibm_is_security_group_rule" "bastion_ingress_ssh_all" {
   group     = "${ibm_is_security_group.bastion.id}"
   direction = "ingress"
   remote    = "${var.remote}"
+
   tcp = {
     port_min = 22
     port_max = 22
   }
 }
+
 resource "ibm_is_security_group_rule" "bastion_egress_ssh_maintenance" {
   group     = "${ibm_is_security_group.bastion.id}"
   direction = "egress"
   remote    = "${ibm_is_security_group.maintenance.id}"
+
   tcp = {
     port_min = 22
     port_max = 22
   }
 }
+
 resource "ibm_is_instance" "bastion" {
   name    = "${var.basename}-bastion-vsi"
   image   = "${var.ibm_is_image_id}"
@@ -48,14 +55,15 @@ resource "ibm_is_instance" "bastion" {
   }
 
   vpc  = "${var.ibm_is_vpc_id}"
-  zone      = "${var.zone}"
-  keys      = ["${var.ibm_is_ssh_key_id}"]
+  zone = "${var.zone}"
+  keys = ["${var.ibm_is_ssh_key_id}"]
 }
 
 resource "ibm_is_floating_ip" "bastion" {
   name   = "${var.basename}-bastion-ip"
   target = "${ibm_is_instance.bastion.primary_network_interface.0.id}"
 }
+
 /*
 # optional
 resource "ibm_is_security_group_rule" "maintenance_egress_443" {
@@ -108,3 +116,4 @@ resource "ibm_is_security_group_rule" "bastion_ingress_icmp_all" {
   }
 }
 */
+
--- vpc-secure-management-bastion-server/tfmodule/output.tf
+++ vpc-secure-management-bastion-server/tfmodule/output.tf
@@ -2,7 +2,8 @@
 output "floating_ip_address" {
   value = "${ibm_is_floating_ip.bastion.address}"
 }
+
 # bastion maintenance security group. Add this to the instances that require access from the bastian
 output security_group_id {
-    value = "${ibm_is_security_group.maintenance.id}"
+  value = "${ibm_is_security_group.maintenance.id}"
 }
