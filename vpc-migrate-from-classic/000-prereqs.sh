53b4a13940d0031bfaa3fab525173eff7f5cc5ee
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2019-08-26 08:31:29 +0200

    more verbose tests

--- vpc-migrate-from-classic/000-prereqs.sh
+++ vpc-migrate-from-classic/000-prereqs.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 set -e
 
-ibmcloud is target --gen 1
+echo ">>> Targeting resource group $RESOURCE_GROUP_NAME..."
 ibmcloud target -g $RESOURCE_GROUP_NAME
+
+echo ">>> Setting VPC target to VPC on Classic..."
+if ibmcloud is >/dev/null; then
+  ibmcloud is target --gen 1
+else
+  echo "Make sure vpc-infrastructure plugin is properly installed with ibmcloud plugin install vpc-infrastructure."
+  exit 1
+fi
+
+echo ">>> Ensuring Cloud Object Storage plugin is installed"
+if ibmcloud cos config list >/dev/null; then
+  echo "cloud-object-storage plugin is OK"
+else
+  echo "Make sure cloud-object-storage plugin is properly installed with ibmcloud plugin install cloud-object-storage."
+  exit 1
+fi
+
+echo ">>> Is terraform installed?"
 terraform version
-ibmcloud cos config list
+
+echo ">>> Is jq (https://stedolan.github.io/jq/) installed?"
+jq -V
+
+echo ">>> Is curl installed?"
+curl -V
