a5b3a4adc3ec0a03c351cbd1895511b41d37d934
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2019-10-01 10:48:49 +0200

    basename was not set in the case where not reusing a vpc [ci skip]

--- vpc-public-app-private-backend/vpc-maintenance.sh
+++ vpc-public-app-private-backend/vpc-maintenance.sh
@@ -11,6 +11,9 @@
 set -e
 set -o pipefail
 
+export instance=$1
+export basename="vpc-pubpriv"
+
 if [ -z "$2" ]; then 
               echo "usage: $0 instance-name (on | off) [prefix [reuse-vpc-name]]"
               exit
@@ -30,17 +33,18 @@ else
     export vpc_name=$4
 fi    
 
-export instance=$1
-export basename="vpc-pubpriv"
-
-
+export instance_name="${prefix}${basename}-${instance}-vsi"
+echo "VPC name is ${vpc_name}"
+echo "Instance name is ${instance_name}"
 
 # Obtain NIC ID for instance
 export NICID=$(ibmcloud is instances --json |\
-       jq -c -r '.[] | select (.vpc.name=="'${vpc_name}'" and .name=="'${prefix}${basename}'-'${instance}'-vsi") | .primary_network_interface.id')
+       jq -c -r '.[] | select (.vpc.name=="'${vpc_name}'" and .name=="'${instance_name}'") | .primary_network_interface.id')
+echo "Network Interface ID is $NICID"
 
 export SGMAINT=$(ibmcloud is security-groups --json |\
        jq -c -r '.[] | select (.vpc.name=="'${vpc_name}'" and .name=="'${prefix}${basename}'-maintenance-sg") | .id')
+echo "Maintenance security group ID is $SGMAINT"
 
 if [ $action = "on" ]; then
     ibmcloud is security-group-network-interface-add $SGMAINT $NICID
