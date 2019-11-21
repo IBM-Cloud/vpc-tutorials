54f885933de5fee21bfb539a7a07f6fa678493d5
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2019-11-21 11:29:24 -0500

    adding cockroach scenario to Travis

---
+++ tests/vpc-cockroachdb-mzr/create-with-terraform.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+set -o pipefail
+this_dir=$(dirname "$0")
+source $this_dir/common.sh
+source $this_dir/../tests_common.sh
+
+ssh_notstrict_config="$(cd $this_dir/../../scripts; pwd -P)"/ssh.notstrict.config
+function testit() {
+    # TODO Testing needs to be a bit more comprehensive
+    APP_URL=$(terraform output APP_URL)
+    test_curl $APP_URL 
+}
+
+# https://www.terraform.io/docs/commands/environment-variables.html#tf_in_automation
+export TF_IN_AUTOMATION=true
+
+# https://www.terraform.io/docs/commands/environment-variables.html#tf_var_name
+export TF_VAR_ibmcloud_api_key=$API_KEY
+export TF_VAR_resources_prefix=at$JOB_ID-
+export TF_VAR_generation=$TARGET_GENERATION
+
+# only use the first key here
+export TF_VAR_ssh_private_key="~/.ssh/id_rsa"
+export TF_VAR_resource_group=$RESOURCE_GROUP
+
+TEST_KEY_NAME=$(ssh_key_name_for_job)
+export TF_VAR_vpc_ssh_keys=[\"$TEST_KEY_NAME\"]
+
+echo "Region is $REGION"
+export TF_VAR_vpc_region=$REGION
+
+cd vpc-cockroachdb-mzr
+
+rm -rf .terraform database-app-mzr.tfstate database-app-mzr.tfstate.backup database-app-mzr.plan
+
+terraform init
+
+terraform plan -state=database-app-mzr.tfstate -out=database-app-mzr.plan
+
+terraform apply -state-out=database-app-mzr.tfstate database-app-mzr.plan
+
+# TODO: Add testing later on
+# testit
+
+terraform destroy -state=database-app-mzr.tfstate
--- tests/vpc-cockroachdb-mzr/teardown.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./delete.sh --template=./vpc-cockroachdb-mzr/vpc-cockroachdb-mzr.test.json --config=./vpc-cockroachdb-mzr/test.json
