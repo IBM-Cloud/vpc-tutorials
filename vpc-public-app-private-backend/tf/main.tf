70f2b18b570f63694b2336703b14ecffb25716c6
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2020-01-17 16:01:48 +0530

    Updates to vpc-pub-priv

--- vpc-public-app-private-backend/tf/main.tf
+++ vpc-public-app-private-backend/tf/main.tf
@@ -1,8 +1,8 @@
 provider "ibm" {
-  region           = "${var.region}"
-  ibmcloud_api_key = "${var.ibmcloud_api_key}"
-  generation       = "${var.generation}"
-  ibmcloud_timeout = "${var.ibmcloud_timeout}"
+  region           = var.region
+  ibmcloud_api_key = var.ibmcloud_api_key
+  generation       = var.generation
+  ibmcloud_timeout = var.ibmcloud_timeout
 }
 
 locals {
@@ -16,6 +16,7 @@ echo "I am the frontend server" > /var/www/html/index.html
 service nginx start
 EOF
 
+
   user_data_backend = <<EOF
 #!/bin/bash
 apt-get update
@@ -23,40 +24,41 @@ apt-get install -y nginx
 echo "I am the backend server" > /var/www/html/index.html
 service nginx start
 EOF
+
 }
 
-module map_gen1_to_gen2 {
-  generation = "${var.generation}"
+module "map_gen1_to_gen2" {
+  generation = var.generation
   source     = "../../tfshared/map-gen1-to-gen2/"
-  image      = "${var.image_name}"
-  profile    = "${var.profile}"
+  image      = var.image_name
+  profile    = var.profile
 }
 
 data "ibm_is_image" "os" {
-  name = "${module.map_gen1_to_gen2.image}"
+  name = module.map_gen1_to_gen2.image
 }
 
-module vpc_pub_priv {
+module "vpc_pub_priv" {
   source              = "../tfmodule"
-  basename            = "${local.BASENAME}"
-  vpc_name            = "${var.vpc_name}"
-  resource_group_name = "${var.resource_group_name}"
-  ssh_key_name        = "${var.ssh_key_name}"
-  zone                = "${var.zone}"
-  backend_pgw         = "${var.backend_pgw}"
-  profile             = "${module.map_gen1_to_gen2.profile}"
-  ibm_is_image_id     = "${data.ibm_is_image.os.id}"
-  maintenance         = "${var.maintenance}"
-  frontend_user_data  = "${local.user_data_frontend}"
-  backend_user_data   = "${local.user_data_backend}"
+  basename            = local.BASENAME
+  vpc_name            = var.vpc_name
+  resource_group_name = var.resource_group_name
+  ssh_key_name        = var.ssh_key_name
+  zone                = var.zone
+  backend_pgw         = var.backend_pgw
+  profile             = module.map_gen1_to_gen2.profile
+  ibm_is_image_id     = data.ibm_is_image.os.id
+  maintenance         = var.maintenance
+  frontend_user_data  = local.user_data_frontend
+  backend_user_data   = local.user_data_backend
 }
 
 locals {
-  bastion_ip = "${module.vpc_pub_priv.bastion_floating_ip_address}"
+  bastion_ip = module.vpc_pub_priv.bastion_floating_ip_address
 }
 
 output "BASTION_IP_ADDRESS" {
-  value = "${local.bastion_ip}"
+  value = local.bastion_ip
 }
 
 output "sshbastion" {
@@ -68,11 +70,11 @@ output "sshfrontend" {
 }
 
 output "FRONT_IP_ADDRESS" {
-  value = "${module.vpc_pub_priv.frontend_floating_ip_address}"
+  value = module.vpc_pub_priv.frontend_floating_ip_address
 }
 
 output "FRONT_NIC_IP" {
-  value = "${module.vpc_pub_priv.frontend_network_interface_address}"
+  value = module.vpc_pub_priv.frontend_network_interface_address
 }
 
 output "sshbackend" {
@@ -80,9 +82,10 @@ output "sshbackend" {
 }
 
 output "BACK_IP_ADDRESS" {
-  value = "${module.vpc_pub_priv.backend_floating_ip_address}"
+  value = module.vpc_pub_priv.backend_floating_ip_address
 }
 
 output "BACK_NIC_IP" {
-  value = "${module.vpc_pub_priv.backend_network_interface_address}"
+  value = module.vpc_pub_priv.backend_network_interface_address
 }
+
