bdc04b3f74b61b6517a760be9b4918ca30a483f3
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2020-04-08 11:23:19 -0400

    Update install.sh
    - Added a timeout to the curl command in order to not wait over 2 minutes for it to timeout naturally.  This was delaying making a required update to the index.html
    
    - closes https://github.ibm.com/bluemix/cloud-portfolio-solutions/issues/1970

--- vpc-app-deploy/shared/install.sh
+++ vpc-app-deploy/shared/install.sh
@@ -6,7 +6,7 @@ indexhtml=/var/www/html/index.html
 
 # Demonstrate the availability of internet repositories.  If www.python.org is availble then other software internet software like
 # npm, pip, docker, ...  if isolated only the software from the ibm mirrors can be accessed
-if curl -o /tmp/x https://www.python.org/downloads/release/python-373/; then
+if curl -o /tmp/x -m 3 https://www.python.org/downloads/release/python-373/; then
     echo INTERNET > $indexhtml
 else
     echo ISOLATED > $indexhtml
