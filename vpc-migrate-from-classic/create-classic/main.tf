0b708fbcf1a8a545f51378c31c843a230e5916e9
Frederic Lavigne <frederic.lavigne@ibm.com>
2020-06-23 11:16:58 -0500

    more tf 0.12 compat

--- vpc-migrate-from-classic/create-classic/main.tf
+++ vpc-migrate-from-classic/create-classic/main.tf
@@ -1,13 +1,13 @@
 provider "ibm" {
-  ibmcloud_api_key      = "${var.ibmcloud_api_key}"
-  iaas_classic_username = "${var.iaas_classic_username}"
-  iaas_classic_api_key  = "${var.iaas_classic_api_key}"
-  region                = "${var.region}"
+  ibmcloud_api_key      = var.ibmcloud_api_key
+  iaas_classic_username = var.iaas_classic_username
+  iaas_classic_api_key  = var.iaas_classic_api_key
+  region                = var.region
 }
 
 resource "ibm_compute_ssh_key" "key" {
   label      = "${var.prefix}-vm-to-migrate"
-  public_key = "${file("${var.ssh_public_key_file}")}"
+  public_key = file(var.ssh_public_key_file)
 }
 
 resource "ibm_compute_vm_instance" "vm" {
@@ -15,7 +15,7 @@ resource "ibm_compute_vm_instance" "vm" {
   domain            = "howto.cloud"
   ssh_key_ids       = ["${ibm_compute_ssh_key.key.id}"]
   os_reference_code = "CENTOS_7_64"
-  datacenter        = "${var.classic_datacenter}"
+  datacenter        = var.classic_datacenter
   cores             = 1
   memory            = 1024
 
@@ -23,9 +23,9 @@ resource "ibm_compute_vm_instance" "vm" {
     connection {
       type        = "ssh"
       user        = "root"
-      private_key = "${file("${var.ssh_private_key_file}")}"
+      private_key = file(var.ssh_private_key_file)
       agent       = false
-      host        = "${ibm_compute_vm_instance.vm.ipv4_address}"
+      host        = ibm_compute_vm_instance.vm.ipv4_address
     }
 
     # install nginx on the server
@@ -40,9 +40,9 @@ resource "ibm_compute_vm_instance" "vm" {
 }
 
 output "CLASSIC_ID" {
-  value = "${ibm_compute_vm_instance.vm.id}"
+  value = ibm_compute_vm_instance.vm.id
 }
 
 output "CLASSIC_IP_ADDRESS" {
-  value = "${ibm_compute_vm_instance.vm.ipv4_address}"
+  value = ibm_compute_vm_instance.vm.ipv4_address
 }
--- vpc-migrate-from-classic/create-vpc-vsi/main.tf
+++ vpc-migrate-from-classic/create-vpc-vsi/main.tf
@@ -1,69 +1,69 @@
 provider "ibm" {
-  ibmcloud_api_key = "${var.ibmcloud_api_key}"
-  region           = "${var.region}"
-  ibmcloud_timeout = "${var.ibmcloud_timeout}"
-  generation       = "${var.generation}"
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.region
+  ibmcloud_timeout = var.ibmcloud_timeout
+  generation       = var.generation
 }
 
 module map_gen1_to_gen2 {
-  generation       = "${var.generation}"
+  generation       = var.generation
   source = "../../tfshared/map-gen1-to-gen2/"
   image = "centos-7.x-amd64"
   profile = "cc1-2x4"
 }
 data "ibm_is_image" "ds_image" {
-  name = "${var.vsi_image_name}"
+  name = var.vsi_image_name
 }
 
 data "ibm_is_ssh_key" "key" {
-  name = "${var.ssh_key_name}"
+  name = var.ssh_key_name
 }
 
 data "ibm_resource_group" "group" {
-  name = "${var.resource_group_name}"
+  name = var.resource_group_name
 }
 
 resource "ibm_is_vpc" "vpc" {
   name           = "${var.prefix}-vpc"
-  resource_group = "${data.ibm_resource_group.group.id}"
+  resource_group = data.ibm_resource_group.group.id
 }
 
 resource "ibm_is_subnet" "subnet" {
   name                     = "${var.prefix}-subnet"
-  vpc                      = "${ibm_is_vpc.vpc.id}"
-  zone                     = "${var.subnet_zone}"
+  vpc                      = ibm_is_vpc.vpc.id
+  zone                     = var.subnet_zone
   total_ipv4_address_count = 64
-  resource_group           = "${data.ibm_resource_group.group.id}"
+  resource_group           = data.ibm_resource_group.group.id
 }
 
 resource "ibm_is_instance" "instance" {
   name           = "${var.prefix}-instance"
-  vpc            = "${ibm_is_vpc.vpc.id}"
-  zone           = "${var.subnet_zone}"
-  profile        = "${module.map_gen1_to_gen2.profile}"
-  image          = "${data.ibm_is_image.ds_image.id}"
+  vpc            = ibm_is_vpc.vpc.id
+  zone           = var.subnet_zone
+  profile        = module.map_gen1_to_gen2.profile
+  image          = data.ibm_is_image.ds_image.id
   keys           = ["${data.ibm_is_ssh_key.key.id}"]
-  resource_group = "${data.ibm_resource_group.group.id}"
+  resource_group = data.ibm_resource_group.group.id
 
   primary_network_interface {
-    subnet = "${ibm_is_subnet.subnet.id}"
+    subnet = ibm_is_subnet.subnet.id
   }
 }
 
 resource "ibm_is_floating_ip" "public_ip" {
   name           = "${var.prefix}-public-ip"
-  target         = "${ibm_is_instance.instance.primary_network_interface.0.id}"
-  resource_group = "${data.ibm_resource_group.group.id}"
+  target         = ibm_is_instance.instance.primary_network_interface.0.id
+  resource_group = data.ibm_resource_group.group.id
 }
 
 resource "ibm_is_security_group" "group" {
   name           = "${var.prefix}-group"
-  resource_group = "${data.ibm_resource_group.group.id}"
-  vpc            = "${ibm_is_vpc.vpc.id}"
+  resource_group = data.ibm_resource_group.group.id
+  vpc            = ibm_is_vpc.vpc.id
 }
 
 resource "ibm_is_security_group_rule" "allow_http" {
-  group     = "${ibm_is_security_group.group.id}"
+  group     = ibm_is_security_group.group.id
   direction = "inbound"
   remote    = "0.0.0.0/0"
 
@@ -74,7 +74,7 @@ resource "ibm_is_security_group_rule" "allow_http" {
 }
 
 resource "ibm_is_security_group_rule" "allow_ssh" {
-  group     = "${ibm_is_security_group.group.id}"
+  group     = ibm_is_security_group.group.id
   direction = "inbound"
   remote    = "0.0.0.0/0"
 
@@ -85,7 +85,7 @@ resource "ibm_is_security_group_rule" "allow_ssh" {
 }
 
 resource "ibm_is_security_group_rule" "allow_ping" {
-  group     = "${ibm_is_security_group.group.id}"
+  group     = ibm_is_security_group.group.id
   direction = "inbound"
   remote    = "0.0.0.0/0"
 
@@ -95,16 +95,16 @@ resource "ibm_is_security_group_rule" "allow_ping" {
 }
 
 resource "ibm_is_security_group_rule" "allow_all" {
-  group     = "${ibm_is_security_group.group.id}"
+  group     = ibm_is_security_group.group.id
   direction = "outbound"
   remote    = "0.0.0.0/0"
 }
 
 resource "ibm_is_security_group_network_interface_attachment" "add_to_group" {
-  security_group    = "${ibm_is_security_group.group.id}"
-  network_interface = "${ibm_is_instance.instance.primary_network_interface.0.id}"
+  security_group    = ibm_is_security_group.group.id
+  network_interface = ibm_is_instance.instance.primary_network_interface.0.id
 }
 
 output "VPC_VSI_IP_ADDRESS" {
-  value = "${ibm_is_floating_ip.public_ip.address}"
+  value = ibm_is_floating_ip.public_ip.address
 }
