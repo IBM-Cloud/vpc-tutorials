d26daa904e881d488e8c6ffa6d1dabf053b22d84
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2020-07-15 14:00:47 -0400

    added single quotes

--- vpc-cockroachdb-mzr/frontend_app.tf
+++ vpc-cockroachdb-mzr/frontend_app.tf
@@ -140,7 +140,7 @@ resource "null_resource" "vsi_app" {
   }
 
   provisioner "local-exec" {
-    command = "echo ${var.ssh_private_key} >> id_rsa"
+    command = "echo '${var.ssh_private_key}' >> id_rsa"
     interpreter = ["bash", "-c"]
   }
 
