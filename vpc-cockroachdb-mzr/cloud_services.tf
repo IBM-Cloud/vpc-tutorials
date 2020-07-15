43a7d7113f85453f2b05ecd8677ba9f726c8d283
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2020-07-15 12:03:52 -0400

    debug: commented out to allow resource deletion

--- vpc-cockroachdb-mzr/cloud_services.tf
+++ vpc-cockroachdb-mzr/cloud_services.tf
@@ -28,21 +28,21 @@ resource "ibm_resource_instance" "cm_certs" {
   resource_group_id = data.ibm_resource_group.group.id
 }
 
-resource "ibm_certificate_manager_import" "cert" {
-  count = 3
+# resource "ibm_certificate_manager_import" "cert" {
+#   count = 3
 
-  certificate_manager_instance_id = ibm_resource_instance.cm_certs.id
-  name                            = element(ibm_is_instance.vsi_database.*.primary_network_interface.0.primary_ipv4_address, count.index)
-  description                     = ""
+#   certificate_manager_instance_id = ibm_resource_instance.cm_certs.id
+#   name                            = element(ibm_is_instance.vsi_database.*.primary_network_interface.0.primary_ipv4_address, count.index)
+#   description                     = ""
 
-  data = {
-    content      = file("config/${var.resources_prefix}-certs/${element(ibm_is_instance.vsi_database.*.primary_network_interface.0.primary_ipv4_address, count.index)}.node.crt")
-    priv_key     = file("config/${var.resources_prefix}-certs/${element(ibm_is_instance.vsi_database.*.primary_network_interface.0.primary_ipv4_address, count.index)}.node.key")
+#   data = {
+#     content      = file("config/${var.resources_prefix}-certs/${element(ibm_is_instance.vsi_database.*.primary_network_interface.0.primary_ipv4_address, count.index)}.node.crt")
+#     priv_key     = file("config/${var.resources_prefix}-certs/${element(ibm_is_instance.vsi_database.*.primary_network_interface.0.primary_ipv4_address, count.index)}.node.key")
     
-    # Terraform does not support running plan if local file does not exist yet, unable to handle setting intermediate to `file("config/ca.crt)`, setting to blank value for now. 
-    intermediate = ""
-  }
+#     # Terraform does not support running plan if local file does not exist yet, unable to handle setting intermediate to `file("config/ca.crt)`, setting to blank value for now. 
+#     intermediate = ""
+#   }
 
-  depends_on = [null_resource.vsi_admin]
+#   depends_on = [null_resource.vsi_admin]
 
-}
+# }
