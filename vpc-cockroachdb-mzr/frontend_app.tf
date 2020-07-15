dbaeb14f1591aa11cb8ae0602cde67cdf6a83e38
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2020-07-15 15:13:49 -0400

    debug: changing to ssh.env from ssh.config

--- vpc-cockroachdb-mzr/frontend_app.tf
+++ vpc-cockroachdb-mzr/frontend_app.tf
@@ -145,7 +145,7 @@ resource "null_resource" "vsi_app" {
   }
 
   provisioner "local-exec" {
-    command = "scp -F ./scripts/ssh.config -i 'id_rsa' -o 'ProxyJump root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}' config/${var.resources_prefix}-certs/client.maxroach.key root@${element(
+    command = "scp -F ./scripts/ssh.env -i 'id_rsa' -o 'ProxyJump root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}' config/${var.resources_prefix}-certs/client.maxroach.key root@${element(
       ibm_is_instance.vsi_app.*.primary_network_interface.0.primary_ipv4_address,
       count.index,
     )}:/vpc-tutorials/sampleapps/nodejs-graphql/certs/client.maxroach.key"
@@ -153,7 +153,7 @@ resource "null_resource" "vsi_app" {
   }
 
   provisioner "local-exec" {
-    command = "scp -F ./scripts/ssh.config -i 'id_rsa' -o 'ProxyJump root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}' config/${var.resources_prefix}-certs/client.maxroach.crt root@${element(
+    command = "scp -F ./scripts/ssh.env -i 'id_rsa' -o 'ProxyJump root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}' config/${var.resources_prefix}-certs/client.maxroach.crt root@${element(
       ibm_is_instance.vsi_app.*.primary_network_interface.0.primary_ipv4_address,
       count.index,
     )}:/vpc-tutorials/sampleapps/nodejs-graphql/certs/client.maxroach.crt"
@@ -161,7 +161,7 @@ resource "null_resource" "vsi_app" {
   }
 
   provisioner "local-exec" {
-    command = "scp -F ./scripts/ssh.config -i 'id_rsa' -o 'ProxyJump root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}' config/${var.resources_prefix}-certs/ca.crt root@${element(
+    command = "scp -F ./scripts/ssh.env -i 'id_rsa' -o 'ProxyJump root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}' config/${var.resources_prefix}-certs/ca.crt root@${element(
       ibm_is_instance.vsi_app.*.primary_network_interface.0.primary_ipv4_address,
       count.index,
     )}:/vpc-tutorials/sampleapps/nodejs-graphql/certs/ca.crt"
---
+++ vpc-cockroachdb-mzr/scripts/ssh.env
@@ -0,0 +1,6 @@
+Host *
+  StrictHostKeyChecking no
+  UserKnownHostsFile=/dev/null
+  LogLevel=quiet
+  BatchMode=yes
+  ConnectTimeout=15
