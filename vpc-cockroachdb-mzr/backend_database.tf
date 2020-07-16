82564e295ed97f4cb1f61608d419bad6b2429738
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2020-07-16 15:39:33 -0400

    fixes for id_rsa

--- vpc-cockroachdb-mzr/backend_database.tf
+++ vpc-cockroachdb-mzr/backend_database.tf
@@ -217,12 +217,12 @@ resource "null_resource" "vsi_database" {
   }
 
   provisioner "local-exec" {
-    command     = "mkdir -p ~/.ssh; echo '${var.ssh_private_key}' >> ~/.ssh/id_rsa_schematics; chmod 600 ~/.ssh/id_rsa_schematics; ls -latr ~/.ssh"
+    command     = "mkdir -p ~/.ssh; echo '${var.ssh_private_key}' > ~/.ssh/id_rsa_schematics; chmod 600 ~/.ssh/id_rsa_schematics; sed -i.bak 's/\r//g' ~/.ssh/id_rsa_schematics"
     interpreter = ["bash", "-c"]
   }
 
   provisioner "local-exec" {
-    command = "scp -F ./scripts/cockroach.txt -i '~/.ssh/id_rsa_schematics' -r root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}:/certs/${element(
+    command = "scp -F ./scripts/ssh-config.txt -i '~/.ssh/id_rsa_schematics' -r root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}:/certs/${element(
       ibm_is_instance.vsi_database.*.primary_network_interface.0.primary_ipv4_address,
       count.index,
     )}.node.key ./config/${var.resources_prefix}-certs/"
@@ -230,7 +230,7 @@ resource "null_resource" "vsi_database" {
   }
 
   provisioner "local-exec" {
-    command = "scp -F ./scripts/cockroach.txt -i '~/.ssh/id_rsa_schematics' -r root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}:/certs/${element(
+    command = "scp -F ./scripts/ssh-config.txt -i '~/.ssh/id_rsa_schematics' -r root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}:/certs/${element(
       ibm_is_instance.vsi_database.*.primary_network_interface.0.primary_ipv4_address,
       count.index,
     )}.node.crt ./config/${var.resources_prefix}-certs/"
@@ -238,7 +238,7 @@ resource "null_resource" "vsi_database" {
   }
 
     provisioner "local-exec" {
-    command = "scp -F ./scripts/cockroach.txt -i '~/.ssh/id_rsa_schematics' -r root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}:/certs/ca.crt ./config/${var.resources_prefix}-certs/"
+    command = "scp -F ./scripts/ssh-config.txt -i '~/.ssh/id_rsa_schematics' -r root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}:/certs/ca.crt ./config/${var.resources_prefix}-certs/"
     interpreter = ["bash", "-c"]
   }
 
@@ -249,7 +249,7 @@ resource "null_resource" "vsi_database" {
   # }
 
   provisioner "local-exec" {
-    command = "scp -F ./scripts/cockroach.txt -i '~/.ssh/id_rsa_schematics' -o 'ProxyJump root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}' config/${var.resources_prefix}-certs/${element(
+    command = "scp -F ./scripts/ssh-config.txt -i '~/.ssh/id_rsa_schematics' -o 'ProxyJump root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}' config/${var.resources_prefix}-certs/${element(
       ibm_is_instance.vsi_database.*.primary_network_interface.0.primary_ipv4_address,
       count.index,
       )}.node.key root@${element(
@@ -260,7 +260,7 @@ resource "null_resource" "vsi_database" {
   }
 
   provisioner "local-exec" {
-    command = "scp -F ./scripts/cockroach.txt -i '~/.ssh/id_rsa_schematics' -o 'ProxyJump root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}' config/${var.resources_prefix}-certs/${element(
+    command = "scp -F ./scripts/ssh-config.txt -i '~/.ssh/id_rsa_schematics' -o 'ProxyJump root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}' config/${var.resources_prefix}-certs/${element(
       ibm_is_instance.vsi_database.*.primary_network_interface.0.primary_ipv4_address,
       count.index,
       )}.node.crt root@${element(
@@ -271,7 +271,7 @@ resource "null_resource" "vsi_database" {
   }
 
   provisioner "local-exec" {
-    command = "scp -F ./scripts/cockroach.txt -i '~/.ssh/id_rsa_schematics' -o 'ProxyJump root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}' config/${var.resources_prefix}-certs/ca.crt root@${element(
+    command = "scp -F ./scripts/ssh-config.txt -i '~/.ssh/id_rsa_schematics' -o 'ProxyJump root@${ibm_is_floating_ip.vpc_vsi_admin_fip[0].address}' config/${var.resources_prefix}-certs/ca.crt root@${element(
       ibm_is_instance.vsi_database.*.primary_network_interface.0.primary_ipv4_address,
       count.index,
     )}:/data/certs/ca.crt"
