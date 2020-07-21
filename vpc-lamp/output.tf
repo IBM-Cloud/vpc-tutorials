71bb0aa403a32784a0bcccf056c9dddf77a16e62
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2020-07-21 11:27:08 -0400

    removed script/ssh.config output

--- vpc-lamp/output.tf
+++ vpc-lamp/output.tf
@@ -4,7 +4,7 @@ output "instance_access" {
   value = <<LAMP
   
   ### SSH into the instance using the following SSH command:
-        ssh -F scripts/ssh.config root@${ibm_is_floating_ip.vpc_vsi_fip.0.address}
+        ssh root@${ibm_is_floating_ip.vpc_vsi_fip.0.address}
   
   --------------------------------------------------------------------------------
     
