4d8fcf44d2cc4f465aaca11bc4ca09c10fab82ad
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2020-07-22 13:02:48 -0400

    removed no longer needed private key

--- vpc-lamp/config-template/lamp.sh
+++ vpc-lamp/config-template/lamp.sh
@@ -4,8 +4,6 @@ export TF_VAR_vpc_ssh_key="your ssh key names in VPC"
 
 export TF_VAR_resource_group="your resource group in VPC"
 
-export TF_VAR_ssh_private_key="~/.ssh/id_rsa"
-
 export TF_VAR_vpc_region="us-south"
 
 export TF_VAR_resources_prefix="ussouthtf"
