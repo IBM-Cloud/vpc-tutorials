be633cb32fdcb1e97a345a899557c3f5598cc2be
Vidyasagar Machupalli <vidyasagar.msc@in.ibm.com>
2020-10-13 13:26:37 +0530

    Added support for Terraform 0.13

--- vpc-cockroachdb-mzr/versions.tf
+++ vpc-cockroachdb-mzr/versions.tf
@@ -1,4 +1,18 @@
-
 terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = ">= 1.12.0"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+    tls = {
+      source = "hashicorp/tls"
+    }
+  }
   required_version = ">= 0.12"
 }
--- vpc-terraform-multicloud/work/versions.tf
+++ vpc-terraform-multicloud/work/versions.tf
@@ -1,3 +1,12 @@
 terraform {
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = ">= 1.12.0"
+    }
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
   required_version = ">= 0.12"
 }
