985ae9a0ddae81fb9f0524cacaaa5735388be32b
Vidyasagar Machupalli <vidyasagar.msc@in.ibm.com>
2020-10-13 12:56:33 +0530

    Upgraded to Terraform 0.13

--- vpc-one-vsi/tf/versions.tf
+++ vpc-one-vsi/tf/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     ibm = {
-      source  = "ibm-cloud/ibm"
+      source = "IBM-Cloud/ibm"
       version = ">= 1.12.0"
     }
   }
--- vpc-public-app-private-backend/tfmodule/versions.tf
+++ vpc-public-app-private-backend/tfmodule/versions.tf
@@ -1,4 +1,9 @@
-
 terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = ">= 1.12.0"
+    }
+  }
   required_version = ">= 0.12"
 }
