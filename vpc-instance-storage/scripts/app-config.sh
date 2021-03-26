4bd7241ad3797e252571d04eca812f86ac23c720
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2021-03-26 08:59:01 -0400

    removed quotes in Requires

--- vpc-instance-storage/scripts/app-config.sh
+++ vpc-instance-storage/scripts/app-config.sh
@@ -25,7 +25,7 @@ function configureServices {
 cat > "/etc/systemd/system/app.service" <<- EOF
     [Unit]
     Description=Simple App Service
-    Requires="network.target data0.mount"
+    Requires=network.target data0.mount
     [Service]
     Type=notify
     ExecStart=/usr/bin/app-service.sh
