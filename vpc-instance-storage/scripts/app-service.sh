941a42621e89955600511cc4d6472aaf17cc782c
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2021-03-26 12:41:39 -0400

    removed extra code not used

--- vpc-instance-storage/scripts/app-service.sh
+++ vpc-instance-storage/scripts/app-service.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-name=app-service
-log_file=$name.$(date +%Y%m%d_%H%M%S).log
-exec 3>&1 1>>$log_file 2>&1
-
-function log_info {
-    printf "\e[1;34m$(date '+%Y-%m-%d %T') %s\e[0m\n" "$@" 1>&3
-}
-
-function log_success {
-    printf "\e[1;32m$(date '+%Y-%m-%d %T') %s\e[0m\n" "$@" 1>&3
-}
-
-function log_warning {
-    printf "\e[1;33m$(date '+%Y-%m-%d %T') %s\e[0m\n" "$@" 1>&3
-}
-
-function log_error {
-    printf >&2 "\e[1;31m$(date '+%Y-%m-%d %T') %s\e[0m\n" "$@" 1>&3
-}
-
 loop_counter=0
 while [ true ]
 do
