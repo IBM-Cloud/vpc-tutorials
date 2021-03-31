1caed8143c411b58291d841bf0f6d2ee96a3eb56
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2021-03-31 17:17:03 -0400

    simplified to smaller number of .tf files

--- vpc-instance-storage/main.tf
+++ vpc-instance-storage/main.tf
@@ -138,3 +138,28 @@ resource "ibm_is_floating_ip" "vpc_vsi_app_fip" {
   target         = ibm_is_instance.vsi_app[0].primary_network_interface[0].id
   resource_group = data.ibm_resource_group.group.id
 }
+
+resource "null_resource" "instance_storage" {
+  connection {
+    type = "ssh"
+    host = ibm_is_floating_ip.vpc_vsi_app_fip.0.address
+    
+    user         = "root"
+    private_key = var.ssh_private_key_format == "file" ? file(var.ssh_private_key_file) : var.ssh_private_key_format == "content" ? var.ssh_private_key_content : tls_private_key.build_key.0.private_key_pem
+  }
+
+  provisioner "file" {
+    content = templatefile("${path.module}/scripts/instance-storage.sh", {})
+    destination = "/usr/bin/instance-storage.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cloud-init status --wait",
+      "chmod +x /usr/bin/instance-storage.sh",
+      "sed -i.bak 's/\r//g' /usr/bin/instance-storage.sh",
+      "systemctl enable instance-storage",      
+      "systemctl start instance-storage",
+    ]
+  }
+}
