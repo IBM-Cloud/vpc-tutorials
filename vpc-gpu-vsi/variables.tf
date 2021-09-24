0cdf230441682f2cacaeb5a5fe6d24b3c5f873e9
Vidyasagar Machupalli <vidyasagar.msc@in.ibm.com>
2021-09-24 12:33:20 +0530

    Re-arranged the variables

--- vpc-gpu-vsi/variables.tf
+++ vpc-gpu-vsi/variables.tf
@@ -1,16 +1,26 @@
-variable "ibmcloud_api_key" {}
+variable "ibmcloud_api_key" {
+  description = "IBM API key. Refer https://cloud.ibm.com/docs/account?topic=account-userapikey&interface=ui"
+}
+
+variable "resource_group_name" {
+  description = "Resource group name."
+}
+
+variable "vpc_name" {
+  description = "Unique name for the VPC."
+}
+
+variable "basename" {
+  description = "Prefix used for all resource names"
+}
+
+variable "ssh_keyname" {
+  description = "SSH key name for VPC. Refer https://cloud.ibm.com/vpc-ext/compute/sshKeys"
+}
 
 variable "ibmcloud_timeout" {
   description = "Timeout for API operations in seconds."
   default     = 900
-}
-
-variable "resource_group_name" {}
-
-variable "vpc_name" {}
-
-variable "basename" {
-  description = "Prefix used for all resource names"
 }
 
 variable "region" {
@@ -21,7 +31,7 @@ variable "subnet_zone" {
   default = "us-south-1"
 }
 
-variable "ssh_keyname" {}
 variable "vsi_image_name" {
-  default = "ibm-ubuntu-20-04-2-minimal-amd64-1"
+  default     = "ibm-ubuntu-20-04-2-minimal-amd64-1"
+  description = "Ubuntu 20.04 images only"
 }
