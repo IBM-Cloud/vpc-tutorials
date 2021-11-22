5dcadfa7b485fd2f6beda86abf7ffec977444c5f
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2021-11-22 09:30:40 -0500

    added comments and re-ordered for reading.

--- vpc-compute-identity/main.tf
+++ vpc-compute-identity/main.tf
@@ -80,18 +80,33 @@ resource "ibm_is_security_group_rule" "ssh" {
   }
 }
 
-# Create a ssh keypair which will be used to provision code onto the system - and also access the VM for debug if needed.
+# Create a ssh keypair which will be used to access the VM for debug if needed.
 resource "tls_private_key" "build_key" {
   algorithm = "RSA"
   rsa_bits = "4096"
 }
 
+# Add the public key in VPC SSH Keys.
 resource "ibm_is_ssh_key" "build_key" {
   name           = "${var.basename}-build-key"
   public_key     = tls_private_key.build_key.public_key_openssh
   resource_group = data.ibm_resource_group.group.id
 }
 
+# Saves the public and private SSH key to local disk for debug use.
+resource "local_file" "build_private_key" {
+  content = tls_private_key.build_key.private_key_pem
+  filename = "local/build_key_rsa"
+  file_permission = "0600"
+}
+
+resource "local_file" "build_public_key" {
+  content = tls_private_key.build_key.public_key_openssh
+  filename = "local/build_key_rsa.pub"
+  file_permission = "0600"
+}
+
+# Create VSI.
 resource "ibm_is_instance" "instance" {
   name           = "${var.basename}-instance"
   vpc            = ibm_is_vpc.vpc.id
@@ -106,6 +121,7 @@ resource "ibm_is_instance" "instance" {
     security_groups = [ibm_is_security_group.group.id]
   }
 
+  # Script file used to create the service on the instance.
   user_data = templatefile("${path.module}/scripts/ssh-authorized-keys-service.sh", {})
 }
 
@@ -114,9 +130,10 @@ resource "ibm_is_floating_ip" "floatingip" {
   target = ibm_is_instance.instance.primary_network_interface[0].id
 }
 
+# Creates the trusted profile and links the VSI created earlier as a compute resource.
 resource "ibm_iam_trusted_profile" "iam_trusted_profile" {
   name = "${var.basename}-trusted-profile"
-  description = "compute identity profile"
+  description = "compute resource trusted profile"
 }
 
 resource "ibm_iam_trusted_profile_link" "iam_trusted_profile_link" {
@@ -128,18 +145,7 @@ resource "ibm_iam_trusted_profile_link" "iam_trusted_profile_link" {
   name = "${var.basename}-trusted-profile-link"
 }
 
-resource "local_file" "build_private_key" {
-  content = tls_private_key.build_key.private_key_pem
-  filename = "local/build_key_rsa"
-  file_permission = "0600"
-}
-
-resource "local_file" "build_public_key" {
-  content = tls_private_key.build_key.public_key_openssh
-  filename = "local/build_key_rsa.pub"
-  file_permission = "0600"
-}
-
+# Creates a policy to allow the newly created instance to read the SSH build key that was added to VPC earlier.
 resource "ibm_iam_trusted_profile_policy" "build_key" {
   profile_id = ibm_iam_trusted_profile.iam_trusted_profile.id
   roles      = ["Viewer"]
@@ -160,11 +166,13 @@ resource "null_resource" "instance_service_init" {
     private_key = tls_private_key.build_key.private_key_pem
   }
 
+  # Deploys the configuration for logrotate to rotate the logs created by the ssh-authorized-keys service. 
   provisioner "file" {
     content = templatefile("${path.module}/scripts/ssh-authorized-keys.conf", {})
     destination = "/etc/logrotate.d/ssh-authorized-keys.conf"
   }
 
+  # Deploys the script that will run as a service and specifies variables such as the trusted profile id and wait time between runs.
   provisioner "file" {
     content = templatefile("${path.module}/scripts/ssh-authorized-keys.sh", {
       profileid = ibm_iam_trusted_profile.iam_trusted_profile.profile_id
@@ -174,6 +182,7 @@ resource "null_resource" "instance_service_init" {
     destination = "/usr/bin/ssh-authorized-keys.sh"
   }
 
+  # Initialize the service and sets the logrotate date: https://serverfault.com/a/497671.
   provisioner "remote-exec" {
     inline = [
       "cloud-init status --wait",
@@ -181,6 +190,8 @@ resource "null_resource" "instance_service_init" {
       "sed -i.bak 's/\r//g' /usr/bin/ssh-authorized-keys.sh",
       "systemctl enable ssh-authorized-keys",
       "systemctl start ssh-authorized-keys",
+      "sleep 30",
+      "logrotate -f /etc/logrotate.d/ssh-authorized-keys.conf"
     ]
   }
 }
