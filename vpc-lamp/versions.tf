147899ac792beb9639e7e1c4d1b07e5e7ae4e6b9
Powell Quiring <powellquiring@gmail.com>
2022-04-14 08:41:42 -0700

    terraform version update

--- vpc-lamp/versions.tf
+++ vpc-lamp/versions.tf
@@ -1,9 +1,9 @@
 terraform {
+  required_version = ">= 1.1.8"
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.12.0"
+      version = ">= 1.40.1"
     }
   }
-  required_version = ">= 0.12"
 }
