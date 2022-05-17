8b31fd539c10f98f21a6e141bf930d6cb00e21fc
Powell Quiring <powellquiring@gmail.com>
2022-05-16 20:10:12 -0700

    bug in 1.40.1 older versions ok

--- vpc-site2site-vpn/versions.tf
+++ vpc-site2site-vpn/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.40.1"
+      version = "< 1.40.1"
     }
   }
   required_version = ">= 1.1.5"
